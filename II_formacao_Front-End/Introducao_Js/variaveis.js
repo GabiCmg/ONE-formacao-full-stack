5184f24f3037a34d3330fb24aa25fbcf292f3d8a
GabiCmg <gabriela.camaroli@gmail.com>
2022-11-23 13:03:25 -0300

    🚀 Projetos-ONE

---
+++ II_formacao_Front-End/Introducao_Js/variaveis.js
@@ -0,0 +1,11 @@
+console.log("Trabalhando com variáveis");
+
+const idade = 26;
+
+console.log(idade);
+
+console.log("A idade de fulano é: ", idade);
+console.log("A idade de fulano a dois anos atrás era: ", idade - 2);
+
+const idadeFutura = idade + 10;
+console.log("A idade de fulano no ano de 2032 será: ", idadeFutura);

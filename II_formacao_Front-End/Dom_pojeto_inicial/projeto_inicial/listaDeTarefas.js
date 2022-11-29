c18d50b2daa646f9efbbd51eecfc2b930fe3e14e
GabiCmg <gabriela.camaroli@gmail.com>
2022-11-29 15:33:18 -0300

    Estilizando botão

--- II_formacao_Front-End/Dom_pojeto_inicial/projeto_inicial/listaDeTarefas.js
+++ II_formacao_Front-End/Dom_pojeto_inicial/projeto_inicial/listaDeTarefas.js
@@ -2,9 +2,20 @@ const criarTarefa = (evento) => {
     // Previniu que o formulário enviasse o dado para algum lugar e mateve a informação sem dar um refresh na página
     evento.preventDefault();
     
+    const lista = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
-    console.log(valor);
+
+    const tarefa = document.createElement('li');
+    // Para acessar a estilização da li
+    tarefa.classList.add('task');
+    // tamplate string (``)
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+
+    tarefa.appendChild(BotaoConclui());
+    lista.appendChild(tarefa);
     input.value = " ";
 };
 
@@ -12,3 +23,17 @@ const novaTarefa = document.querySelector('[data-form-button]');
 
 // console.log(novaTarefa);
 novaTarefa.addEventListener('click', criarTarefa);
+
+// CRIAR BOTÃO
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');
+
+    botaoConclui.classList.add('check-button');
+    
+    botaoConclui.addEventListener('click', () => {
+        console.log('fui clicado');
+    });
+
+    return botaoConclui;
+}

76b78be3d013fd9ba6eb3e173d26d97e7293e095
GabiCmg <gabriela.camaroli@gmail.com>
2022-11-29 10:53:18 -0300

    🚀 Projetos-ONE

--- II_formacao_Front-End/Dom_pojeto_inicial/projeto_inicial/listaDeTarefas.js
+++ II_formacao_Front-End/Dom_pojeto_inicial/projeto_inicial/listaDeTarefas.js
@@ -1,4 +1,14 @@
+const criarTarefa = (evento) => {
+    // Previniu que o formulário enviasse o dado para algum lugar e mateve a informação sem dar um refresh na página
+    evento.preventDefault();
+    
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+    console.log(valor);
+    input.value = " ";
+};
+
 const novaTarefa = document.querySelector('[data-form-button]');
 
 // console.log(novaTarefa);
-novaTarefa.addEventListener('click', () => {console.log('fui clicado!')});
+novaTarefa.addEventListener('click', criarTarefa);
